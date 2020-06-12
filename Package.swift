c2b19ced8fd9a9a2988423ab2ec73d8fe1b34cc1
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-12 14:38:35 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Zephyr",
     products: [.library(name: "Zephyr", targets: ["Zephyr"])],
-    targets: [.target(name: "Zephyr", path: "Sources")]
+    targets: [.target(name: "Zephyr", path: "Sources")],
+    platforms: [.iOS(.v8)],  
+    swiftLanguageVersions: [.v5]
 )
