8e729807025b2c0c79386c2ec574c8c03f84dd0d
Shaun Donnelly <shaundonnelly@gmail.com>
2024-03-19 08:22:59 +0000

    Add visionOS support to podspec and spm

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Zephyr",
-    platforms: [.iOS(.v11), .tvOS(.v11), .watchOS("9.0")],
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v9), .visionOS(.v1)],
     products: [.library(name: "Zephyr", targets: ["Zephyr"])],
     targets: [.target(name: "Zephyr", path: "Sources")],  
     swiftLanguageVersions: [.v5]
--- Zephyr.podspec
+++ Zephyr.podspec
@@ -17,6 +17,7 @@ Pod::Spec.new do |s|
   s.ios.deployment_target      = '11.0'
   s.tvos.deployment_target     = '11.0'
   s.watchos.deployment_target  = '9.0'
+  s.visionos.deployment_target  = '1.0'
 
   # Sources
   s.source       = { :git => "https://github.com/ArtSabintsev/Zephyr.git", :tag => s.version.to_s }
